224ab1b233997dcbd0854ed679274ac1331755e8
Brandon McConnell <brandon@dreamthinkbuild.com>
2023-06-14 03:17:40 -0700

    Replace `replaceAll` usages with regex global flags

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = plugin(({ matchUtilities }) => {
   matchUtilities({
     multi: (value) => {
       const escape = (str) => {
-        return str.replaceAll('_', '\\_').replaceAll(' ', '_')
+        return str.replace(/_/g, '\\_').replace(/ /g, '_')
       }
       const utilities = value.split(';').map(escape).join(' ')
       return {

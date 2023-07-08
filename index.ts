2ed08140829d6b1292cc23baa68a13df5b2c4983
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2023-07-08 16:11:57 +0530

    src to typescript

---
+++ index.ts
@@ -0,0 +1,16 @@
+import plugin from 'tailwindcss/plugin';
+
+module.exports = plugin(({ matchUtilities }) => {
+  matchUtilities({
+    multi: (value) => {
+      const escape = (str: string) => {
+        return str.replace(/_/g, '\\_').replace(/ /g, '_');
+      };
+
+      const utilities = value.split(';').map(escape).join(' ');
+      return {
+        [`@apply ${utilities}`]: {},
+      };
+    },
+  });
+});

3f79e7da71ae7e39badb42f4cf9c5e9c24b7129a
Brandon McConnell <brandon@dreamthinkbuild.com>
2024-04-19 05:03:08 -0700

    Add nesting support

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -7,10 +7,9 @@ const plugin_1 = __importDefault(require("tailwindcss/plugin"));
 exports.default = (0, plugin_1.default)(({ matchUtilities }) => {
     matchUtilities({
         multi: (value) => {
-            const escape = (str) => {
-                return str.replace(/_/g, '\\_').replace(/ /g, '_');
-            };
-            const utilities = value.slice(1, -1).split(';').map(escape).join(' ');
+            const escape = (str) => str.replace(/_/g, '\\_').replace(/ /g, '_');
+            const delimiter = /;(?![^[]*\])/;
+            const utilities = value.slice(1, -1).split(delimiter).map(escape).join(' ');
             return !utilities.trim()
                 ? {}
                 : {
--- index.ts
+++ index.ts
@@ -3,10 +3,9 @@ import plugin from 'tailwindcss/plugin';
 export default plugin(({ matchUtilities }) => {
   matchUtilities({
     multi: (value) => {
-      const escape = (str: string) => {
-        return str.replace(/_/g, '\\_').replace(/ /g, '_');
-      };
-      const utilities = value.slice(1, -1).split(';').map(escape).join(' ');
+      const escape = (str: string) => str.replace(/_/g, '\\_').replace(/ /g, '_');
+      const delimiter = /;(?![^[]*\])/;
+      const utilities = value.slice(1, -1).split(delimiter).map(escape).join(' ');
       return !utilities.trim()
         ? {}
         : {

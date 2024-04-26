204cc824edc00e1dd438b24c533120b78fcc490e
Brandon McConnell <brandon@dreamthinkbuild.com>
2024-04-26 12:27:31 -0700

    Throw more descriptive error when invalid multi value is provided

--- index.ts
+++ index.ts
@@ -3,6 +3,13 @@ import plugin from 'tailwindcss/plugin.js';
 export default plugin(({ matchUtilities }) => {
   matchUtilities({
     multi: (value) => {
+      const ends = [value[0], value.slice(-1)[0]];
+      if (!ends.every((end) => end && [`'`, `"`].includes(end))) {
+        throw new Error(
+          `Invalid multi value. \`${value}\` must be quoted with single or double quotes (e.g. \`multi-['...']'\`).`
+        );
+      }
+
       const escape = (str: string) => str.replace(/_/g, '\\_').replace(/ /g, '_');
       const delimiter = /;(?![^[]*\])/;
       const utilities = value
